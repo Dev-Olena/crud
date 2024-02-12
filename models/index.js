e971592805744080f48faf5c9297958df3922d0a
Dev-Olena <alenagruda@gmail.com>
2024-02-12 11:46:58 +0200

    refactor models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const {Client} = require('pg');
 const config = require('../config.json');
-const Cat = require('./Cat');
-const Owner = require('./Owner');
+// const Cat = require('./Cat');
+// const Owner = require('./Owner');
+const fs = require('fs');
+const path = require('path');
 
 const mode = process.env.NODE_ENV || 'development';
 
@@ -10,13 +12,27 @@ const devConf = config[mode];
 const client = new Client(devConf);
 client.connect();
 
-Cat._client = client;
-Owner._client = client;
+// Cat._client = client;
+// Owner._client = client;
 
+const currentFileName = path.basename(__filename);
 
-module.exports = {
-    client,
-    Cat,
-    Owner
-}
+const db = {
+    client
+};
+
+fs.readdirSync(__dirname)
+            .filter(fileName => /.js$/.test(fileName) && fileName !== currentFileName)
+            .forEach(fileName => {
+                const absPathToFile = path.join(__dirname, fileName);
+                const Model = require(absPathToFile);
+                Model._client = client;
+                db[Model.name] = Model;
+            })
+module.exports = db;
+// module.exports = {
+//     client,
+//     Cat,
+//     Owner
+// }
 
